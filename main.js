fd155ebbf962d73866fda00bb08a6f10543e6ea0
suni2010 <90258867+suni2010@users.noreply.github.com>
2022-10-06 19:33:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ y=0;
 draw_circle="";
 draw_rect="";
 var SpeechRecognition = window.webkitSpeechRecognition;
-var recognition=new SpeechRecognition();
+var recognition = new SpeechRecognition();
 function start(){
-    document.getElementById('status').innerHTML="Systen is listening please speak";
+    document.getElementById('status').innerHTML="System is listening please speak";
     recognition.start();
 }
 recognition.onresult=function (event){
     console.log(event);
-    var content=event.resultsa[0][0].transcript;
+    var content=event.results[0][0].transcript;
     document.getElementById('status').innerHTML="The speech has been recognised as"+content;
     if (content=="circle"){
       x=Math.floor(Math.random()*900);
